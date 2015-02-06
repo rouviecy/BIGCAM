ac06468ec5060873de7e0485a6da0998a4c6e48c
rouviecy <rouvcyril@yahoo.fr>
2015-02-06 16:12:23 +0100

    Other encoders and decoders

--- src/utils/Modem.h
+++ src/utils/Modem.h
@@ -13,13 +13,18 @@
 
 #include <string>
 
+typedef struct{float a; float b;} struct_2f;
 typedef struct{float a; float b; float c;} struct_3f;
 
 class Modem{
 
 public:
 
+	static std::string Enc_1f(float a);
+	static std::string Enc_2f(float a, float b);
 	static std::string Enc_3f(float a, float b, float c);
+	static float Dec_1f(std::string message);
+	static struct_2f Dec_2f(std::string message);
 	static struct_3f Dec_3f(std::string message);
 
 };
