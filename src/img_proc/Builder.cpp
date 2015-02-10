06794f08a2eef1ce8cda55cbf1e8bb3562c00b23
rouviecy <rouvcyril@yahoo.fr>
2015-02-10 22:46:54 +0100

    Code factorization

--- src/img_proc/Builder.cpp
+++ src/img_proc/Builder.cpp
@@ -12,32 +12,21 @@ void Builder::Retrieve_pictures(){
 
 void Builder::Build(){
 	for(size_t i = 0; i < list_img.size(); i++){
-		int w = list_img[i].img.rows;
-		int h = list_img[i].img.cols;
-		list_img[i].x_map = (int) ((float) w * list_img[i].x / 2);
-		list_img[i].y_map = (int) ((float) h * list_img[i].y) / 2;
+		list_img[i].x_map = (int) ((float) list_img[i].img.rows * list_img[i].x / 2.);
+		list_img[i].y_map = (int) ((float) list_img[i].img.cols * list_img[i].y / 2.);
 	}
-	int x_offset = list_img[0].x_map;
-	int y_offset = list_img[0].y_map;
-	int x_max = x_offset;
-	int y_max = y_offset;
+	int x_offset = list_img[0].x_map; int x_max = x_offset;
+	int y_offset = list_img[0].y_map; int y_max = y_offset;
 	for(size_t i = 0; i < list_img.size(); i++){
 		if(x_offset > list_img[i].x_map){x_offset = list_img[i].x_map;}
 		if(y_offset > list_img[i].y_map){y_offset = list_img[i].y_map;}
 		if(list_img[i].x_map + list_img[i].img.cols > x_max){x_max = list_img[i].x_map + list_img[i].img.cols;}
 		if(list_img[i].y_map + list_img[i].img.rows > y_max){y_max = list_img[i].y_map + list_img[i].img.rows;}
 	}
+	cv::Mat bigmap = cv::Mat::zeros(y_max - y_offset, x_max - x_offset, CV_8UC3);
 	for(size_t i = 0; i < list_img.size(); i++){
-		list_img[i].x_map -= x_offset;
-		list_img[i].y_map -= y_offset;
-	}
-	x_max -= x_offset;
-	y_max -= y_offset;
-	cv::Mat bigmap = cv::Mat::zeros(y_max, x_max, CV_8UC3);
-	for(size_t i = 0; i < list_img.size(); i++){
-		cv::Rect roi(cv::Point(list_img[i].x_map, list_img[i].y_map), cv::Size(list_img[i].img.cols, list_img[i].img.rows));
-		cv::Mat destinationROI = bigmap(roi);
-		list_img[i].img.copyTo(destinationROI);
+		cv::Rect roi(cv::Point(list_img[i].x_map - x_offset, list_img[i].y_map - y_offset), list_img[i].img.size());
+		list_img[i].img.copyTo(bigmap(roi));
 	}
 	cv::imwrite("test/map.png", bigmap);
 }
