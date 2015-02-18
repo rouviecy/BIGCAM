51f23a6a55c053d91d12b1550a49e9bd44e10620
rouviecy <rouvcyril@yahoo.fr>
2015-02-18 22:10:40 +0100

    Code factorization

--- src/img_proc/Builder.cpp
+++ src/img_proc/Builder.cpp
@@ -15,22 +15,12 @@ void Builder::Build(){
 		cv::Point img_center(list_img[i].img.cols, list_img[i].img.rows);
 		cv::Mat mat_rot = cv::getRotationMatrix2D(img_center, - list_img[i].thz * 57.296, 1.);
 		cv::Mat mat_transl = cv::Mat(cv::Size(3, 2), CV_32FC1);
-		mat_transl.at<float>(0, 0) = (float) 1;
-		mat_transl.at<float>(0, 1) = (float) 0;
-		mat_transl.at<float>(0, 2) = (float) list_img[i].img.cols / 2;
-		mat_transl.at<float>(1, 0) = (float) 0;
-		mat_transl.at<float>(1, 1) = (float) 1;
-		mat_transl.at<float>(1, 2) = (float) list_img[i].img.rows / 2;
-		vector <cv::Point> vertices;
-		vertices.push_back(cv::Point(0,						0));
-		vertices.push_back(cv::Point(list_img[i].img.rows,	0));
-		vertices.push_back(cv::Point(list_img[i].img.rows,	list_img[i].img.cols));
-		vertices.push_back(cv::Point(0,						list_img[i].img.cols));
+		mat_transl.at<float>(0, 0) = 1.; mat_transl.at<float>(0, 1) = 0.; mat_transl.at<float>(0, 2) = (float) list_img[i].img.cols / 2;
+		mat_transl.at<float>(1, 0) = 0.; mat_transl.at<float>(1, 1) = 1.; mat_transl.at<float>(1, 2) = (float) list_img[i].img.rows / 2;
 		list_img[i].rotation_rect.center = img_center;
 		list_img[i].rotation_rect.angle = list_img[i].thz * 57.296;
-		list_img[i].rotation_rect.size = cv::Size(list_img[i].img.cols, list_img[i].img.rows);
+		list_img[i].rotation_rect.size = cv::Size(list_img[i].img.cols - 2, list_img[i].img.rows - 2);
 		cv::Size new_size(list_img[i].img.cols * 2, list_img[i].img.rows * 2);
-		cv::Mat tempo = cv::Mat::zeros(new_size, CV_8UC3);
 		cv::warpAffine(list_img[i].img, list_img[i].img, mat_transl, new_size);
 		cv::warpAffine(list_img[i].img, list_img[i].img, mat_rot, new_size, CV_INTER_LINEAR);
 		list_img[i].x_map = (int) ((float) list_img[i].img.rows * list_img[i].x / 10.);
@@ -50,14 +40,9 @@ void Builder::Build(){
 		vector <cv::Point> poly_vertices, poly_roi;
 		cv::Point2f rect_vertices[4];
 		list_img[i].rotation_rect.points(rect_vertices);
-		cv::Point vertice_A(list_img[i].x_map - x_offset + rect_vertices[0].x, list_img[i].y_map - y_offset + rect_vertices[0].y);
-		cv::Point vertice_B(list_img[i].x_map - x_offset + rect_vertices[1].x, list_img[i].y_map - y_offset + rect_vertices[1].y);
-		cv::Point vertice_C(list_img[i].x_map - x_offset + rect_vertices[2].x, list_img[i].y_map - y_offset + rect_vertices[2].y);
-		cv::Point vertice_D(list_img[i].x_map - x_offset + rect_vertices[3].x, list_img[i].y_map - y_offset + rect_vertices[3].y);
-		poly_vertices.push_back(vertice_A);
-		poly_vertices.push_back(vertice_B);
-		poly_vertices.push_back(vertice_C);
-		poly_vertices.push_back(vertice_D);
+		for(size_t j = 0; j < 4; j++){
+			poly_vertices.push_back(cv::Point(list_img[i].x_map - x_offset + rect_vertices[j].x, list_img[i].y_map - y_offset + rect_vertices[j].y));
+		}
 		cv::approxPolyDP(poly_vertices, poly_roi, 1.0, true);
 		cv::fillConvexPoly(mask, &poly_roi[0], poly_roi.size(), cv::Scalar(255, 255, 255), 8, 0);
 		cv::Mat new_layer = cv::Mat::zeros(y_max - y_offset, x_max - x_offset, CV_8UC3);
