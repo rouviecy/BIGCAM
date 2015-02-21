3b1a7b7cd2424108f0eced39023d2cd45bb99bb2
rouviecy <rouvcyril@yahoo.fr>
2015-02-21 11:47:40 +0100

    Added tracking between two consecutive images

--- src/img_proc/Builder.cpp
+++ src/img_proc/Builder.cpp
@@ -29,6 +29,33 @@ void Builder::Build(){
 	for(size_t i = 0; i < list_img.size(); i++){
 		cv::Point position(list_img[i].x_map - x_offset, list_img[i].y_map - y_offset);
 		Mask::Pick_and_place(list_img[i].img, bigmap, position, list_img[i].thz * 57.296);
+		tracking.Set_img_next(list_img[i].img);
+		if(i > 0){
+			tracking.Set_img_prev(list_img[i - 1].img);
+			tracking.GoodFeatures(50);
+			struct_link_amers new_link;
+			if(tracking.Tracker()){
+				new_link.amers_prev = tracking.Get_amers();
+				new_link.amers_next = tracking.Get_nv();
+				new_link.x_origin_prev = list_img[i - 1].x_map - x_offset + list_img[i - 1].img.cols / 2;
+				new_link.y_origin_prev = list_img[i - 1].y_map - y_offset + list_img[i - 1].img.rows / 2;
+				new_link.thz_origin_prev = -list_img[i - 1].thz;
+				new_link.x_origin_next = list_img[i].x_map - x_offset + list_img[i].img.cols / 2;
+				new_link.y_origin_next = list_img[i].y_map - y_offset + list_img[i].img.rows / 2;
+				new_link.thz_origin_next = -list_img[i].thz;
+				amers.push_back(new_link);
+			}
+		}
+	}
+	for(size_t i = 0; i < amers.size(); i++){
+		for(size_t j = 0; j < amers[i].amers_next.size(); j++){
+			// TODO : corriger erreur de formule ...
+			int x1 = amers[i].x_origin_prev + (int) ((float) +cos(amers[i].thz_origin_prev) * (float) amers[i].amers_prev[j].x + (float) -sin(amers[i].thz_origin_prev) * (float) amers[i].amers_prev[j].y);
+			int y1 = amers[i].y_origin_prev + (int) ((float) +sin(amers[i].thz_origin_prev) * (float) amers[i].amers_prev[j].x + (float) +cos(amers[i].thz_origin_prev) * (float) amers[i].amers_prev[j].y);
+			int x2 = amers[i].x_origin_next + (int) ((float) +cos(amers[i].thz_origin_next) * (float) amers[i].amers_next[j].x + (float) -sin(amers[i].thz_origin_next) * (float) amers[i].amers_next[j].y);
+			int y2 = amers[i].y_origin_next + (int) ((float) +sin(amers[i].thz_origin_next) * (float) amers[i].amers_next[j].x + (float) +cos(amers[i].thz_origin_next) * (float) amers[i].amers_next[j].y);
+			cv::line(bigmap, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 0, 255), 1);
+		}
 	}
 	cv::imwrite("test/map.png", bigmap);
 }
--- src/img_proc/Builder.h
+++ src/img_proc/Builder.h
@@ -16,6 +16,19 @@
 #include "Tracking.h"
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <cmath>
+
+typedef struct{
+	int index;
+	int x_origin_prev;
+	int y_origin_prev;
+	float thz_origin_prev;
+	int x_origin_next;
+	int y_origin_next;
+	float thz_origin_next;
+	std::vector <cv::Point2i> amers_prev;
+	std::vector <cv::Point2i> amers_next;
+} struct_link_amers;
 
 class Builder{
 
@@ -27,12 +40,16 @@ public:
 
 private:
 
+
+
 	#define MARGIN_X 1000
 	#define MARGIN_Y 1000
 
 	Io_file io_file;
 	Tracking tracking;
 	std::vector <struct_img> list_img;
+	
+	std::vector <struct_link_amers> amers;
 
 };
 
