e2210813aa1955a1c791c10feb62567479a29052
rouviecy <rouvcyril@yahoo.fr>
2015-02-21 10:31:45 +0100

    Updated model

--- src/img_proc/Utils.h
+++ src/img_proc/Utils.h
@@ -1,6 +1,6 @@
 /*
  * @(#)		Utils.h
- * @version	1.4
+ * @version	1.5
  * @autor	C. Rouvière
  */
 
@@ -20,7 +20,6 @@ class Utils{
 
 public:
 
-	static char* string_index(const std::string racine, const int index);
 	static int Distance_carree(int x1, int y1, int x2, int y2);
 	static bool In_img(cv::Point pt, int max_x, int max_y);
 	static std::vector <int> Rot2D(int x, int y, int theta);
